7fa9f5dc20245eb6b4a45a75a24b043d7d7a8de1
webdev-faizan <webdev.faizanali@gmail.com>
2024-05-15 09:47:45 -0400

    More secure implementation of protected routes

--- middlewares/ProtectRoutes.js
+++ middlewares/ProtectRoutes.js
@@ -8,18 +8,29 @@ const ProtectRoutes = async (req, resp, next) => {
       req.headers.authorization.startsWith("Bearer")
     ) {
       const token = req.headers.authorization.split(" ")[1];
-      const userInfo = await jwtDecodes(token);
       const existing_user = await userModels.findById({ _id: userInfo.id });
+      const userInfo = Jwt.verify(token, process.env.JWT_SECRET);
       if (existing_user) {
-        req.user = existing_user.id;
-        next();
+        if (userInfo.iat > userInfo.passwordChangeAt) {
+          req.user = existing_user.id;
+          next();
+        }
+        resp.clearCookie("auth");
+        resp.redirect("/login");
+        return resp.status(401).json({ message: "Session is Expired" });
       } else {
+        resp.clearCookie("auth");
+        resp.redirect("/login");
         return resp.status(403).json({ message: "User not found" });
       }
     } else {
+      resp.clearCookie("auth");
+      resp.redirect("/login");
       return resp.status(403).json({ message: "invalid token or expire" });
     }
   } catch (error) {
+    resp.clearCookie("auth");
+    resp.redirect("/login");
     return resp.status(5000).json({ message: "Internal server error" });
   }
 };
