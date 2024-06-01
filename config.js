f0e00eb9e4cba752f44e6c174f8208d17a9711df
webdev-faizan <webdev.faizanali@gmail.com>
2024-06-01 13:56:30 -0400

    add env base url orign

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
 const Limit = {
   windowMs: 15 * 60 * 1000,
-  limit: 100,
+  limit: 10,
 };
-
 const Cors = {
-  origin: "*",
+  origin: process.env.BASE_URL,
   methods: ["GET", "POST", "PUT"],
 };
 
